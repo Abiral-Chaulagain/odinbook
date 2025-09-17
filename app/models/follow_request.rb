3a76ba0771254a94e8dba3092e2c6bffca079206
Abiral-Chaulagain <abiral.c@hotmail.com>
2025-09-17 07:48:46 +0545

    follow request model fix

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -2,7 +2,7 @@ class FollowRequest < ApplicationRecord
   enum status: { pending: 0, accepted: 1, declined: 2 }
 
   belongs_to :follower, class_name: "User"
-  belongs_to :followed, class_name: "User"
+  belongs_to :followee, class_name: "User"
 
-  validates :follower_id, uniqueness: { scope: :followed_id }
+  validates :follower_id, uniqueness: { scope: :followee_id }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-devise_for :users
+  devise_for :users
 
-root to: "posts#index"
+  root to: "posts#index"
 
-resources :follow_requests, only: [ :create, :update, :destroy ]
-resources :users, only: [ :index, :show ]
+  resources :follow_requests, only: [ :create, :update, :destroy ]
+  resources :users, only: [ :index, :show ]
 
-resources :posts do
-  resources :comments, only: [ :create, :destroy ]
-  resources :likes, only: [ :create, :destroy ]
-end
+  resources :posts do
+    resources :comments, only: [ :create, :destroy ]
+    resources :likes, only: [ :create, :destroy ]
+  end
 end
