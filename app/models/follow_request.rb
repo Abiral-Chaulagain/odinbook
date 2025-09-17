b8f4de80b4802998d8e3dd517a657382cb293b0a
Abiral-Chaulagain <abiral.c@hotmail.com>
2025-09-17 09:23:38 +0545

    added clearing cache

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -1,10 +1,8 @@
 class FollowRequest < ApplicationRecord
+  enum status: { pending: 0, accepted: 1, declined: 2 }
+
   belongs_to :follower, class_name: "User"
   belongs_to :followee, class_name: "User"
 
   validates :follower_id, uniqueness: { scope: :followee_id }
-
-  class_eval do
-    enum status: { pending: 0, accepted: 1, declined: 2 }
-  end
 end
