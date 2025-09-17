a814d32857968f1d9ce23515dd8b8e619365f3b9
Abiral-Chaulagain <abiral.c@hotmail.com>
2025-09-17 07:42:42 +0545

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Odinbook
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    Rails.logger.info ">>> Booting with Rails #{Rails.version}"
   end
 end
