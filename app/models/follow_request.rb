3b7a382d5891f1cd9c11aeb04c297f019991aeed
Abiral-Chaulagain <abiral.c@hotmail.com>
2025-09-17 10:19:18 +0545

    follow_request enum changes

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -1,7 +1,7 @@
 require "active_record/enum"
 
 class FollowRequest < ApplicationRecord
-  enum status: { pending: 0, accepted: 1, declined: 2 }
+  # enum status: { pending: 0, accepted: 1, declined: 2 }
 
   belongs_to :follower, class_name: "User"
   belongs_to :followee, class_name: "User"
